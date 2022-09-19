66b720aadeb085f5376c42c6efdd2d72631aba53
Martin Lazarevski <91286843+Maaczzy@users.noreply.github.com>
2022-09-19 15:24:28 +0200

    add laravel - part3

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{config('app.name', 'BW')}}</title>
+</head>
+<body>
+    <h1>Welcome to Services</h1>
+    <p>This is the services page.</p>
+</body>
+</html>
